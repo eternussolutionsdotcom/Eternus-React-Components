0840a2b730401994ddfac521269ef63dec762d09
Vivekanand Kedar <vivekanand.kedar@gmail.com>
2019-06-12 14:44:34 +0530

    Commiting initial FileUpload compnent

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -2,10 +2,25 @@ import React from 'react'
 
 const FileUpload = props => {
     return (
-        <div>
-            
+        <div className="input-group">
+            <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroupFileAddon01">
+                    Upload
+    </span>
+            </div>
+            <div className="custom-file">
+                <input
+                    type="file"
+                    className="custom-file-input"
+                    id="inputGroupFile01"
+                    aria-describedby="inputGroupFileAddon01"
+                />
+                <label className="custom-file-label" htmlFor="inputGroupFile01">
+                    Choose file
+    </label>
+            </div>
         </div>
     )
 }
 
-export {FileUpload}
+export { FileUpload }
