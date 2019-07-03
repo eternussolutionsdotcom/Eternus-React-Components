b675335998098d598f9c86ddbde9b56e3321c938
Snehal Patil <snehal.patil@Eternus.local>
2019-07-03 17:28:14 +0530

    -Pradnya file changes

--- src/components/Menu/Navbar.js
+++ src/components/Menu/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Row, Col } from 'react-bootstrap';
-import profile from "../../../main/src/profile.png";
+import profile from "./profile.png";
 
 
 
